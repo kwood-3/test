3fe54fb5bf929f3431134f947284d21f24880888
kwood-3 <r.keith.w@gmail.com>
2015-10-05 13:49:23 -0400

    seeing if it will show

---
+++ HelloWorld2/src/com/krw/SayHello.java
@@ -0,0 +1,23 @@
+package com.krw;
+
+public class SayHello 
+{
+
+	public static void main(String[] args) throws InterruptedException 
+	{
+		boolean check = true;
+		int count = 1;
+		while (check)
+		{
+			// TODO Auto-generated method stub
+			System.out.println("Hello world " + count);
+			
+			Thread.sleep(1000);
+			count++;
+			//if (count==11) check = false;
+		}
+		System.out.println ("Goodbye you cruel world");
+		
+	}
+
+}
