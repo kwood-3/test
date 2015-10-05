4fd212bfc158417c895adda667212736b06e8fea
kwood-3 <r.keith.w@gmail.com>
2015-10-05 11:20:41 -0400

    This is my first commit

---
+++ HelloWorld/src/krw/SayHello.java
@@ -0,0 +1,18 @@
+package krw;
+
+public class SayHello 
+{
+
+	public static void main(String[] args) throws InterruptedException 
+	{
+		while (true)
+		{
+			// TODO Auto-generated method stub
+			System.out.println("hello world");
+			
+			Thread.sleep(1000);
+		}
+		
+	}
+
+}
